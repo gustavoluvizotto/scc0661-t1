7bbf04983c32a081c53b9e72f0c71b689af90876
gustavoluvizotto@gmail.com <gustavoluvizotto@gmail.com@18ec1079-f43a-f6d9-b6ee-906adbc819b7>
2013-08-14 00:09:13 +0000

    Versão 1.0
    - iniciando google code e projeto...

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: gustavo
+ *
+ * Created on August 13, 2013, 8:53 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * testando google code
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
